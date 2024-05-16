6104a39a8ef0fa3b1184938fec3b4fb71398a136
Taui Silva <tauisilva@gmail.com>
2024-05-15 21:53:24 -0300

    feat: criado design, iniciado logica de semaforo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,32 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'semaforo-app';
+
+  color: { [key: string]: boolean } = {
+    red: false,
+    orange: false,
+    green: false,
+  };
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.cicloSemafaro();
+      console.log(this.color)
+    }, 2000)
+  }
+
+  cicloSemafaro() {
+    Object.keys(this.color).forEach((key, index) => {
+      if (index === 0) {
+        if (!this.color[key]) {
+          this.color[key] = true;
+        } else {
+          this.color['green'] = true;
+          this.color['red'] = false;
+        }
+      }
+    })
+  }
 }
