0ce2b2ef1a114fd541edd107754c7ac8e5a0a1bb
Taui Silva <tauisilva@gmail.com>
2024-05-16 21:22:40 -0300

    fix: logica do semaforo, style: button de start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,65 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'semaforo-app';
+  init = false;
 
-  color: { [key: string]: boolean } = {
-    red: false,
-    orange: false,
-    green: false,
-  };
-
+  colors = [
+    { color: 'red', active: false },
+    { color: 'orange', active: false },
+    { color: 'green', active: false },
+  ]
   ngOnInit(): void {
-    setTimeout(() => {
-      this.cicloSemafaro();
-      console.log(this.color)
-    }, 2000)
+
+  }
+  initSemaforo() {
+    if (this.init) {
+      this.cicloSemaforo(); // Inicia o ciclo imediatamente
+      setInterval(() => {
+        this.cicloSemaforo();
+      }, 5000); // Ciclo a cada 3 segundos
+    } else {
+      this.colors.forEach(cor => {
+        cor.active = false;
+      });
+    }
   }
 
-  cicloSemafaro() {
-    Object.keys(this.color).forEach((key, index) => {
-      if (index === 0) {
-        if (!this.color[key]) {
-          this.color[key] = true;
-        } else {
-          this.color['green'] = true;
-          this.color['red'] = false;
-        }
-      }
-    })
+  cicloSemaforo() {
+    const activeIndex = this.colors.findIndex(cor => cor.active);
+    if (activeIndex === -1) {
+      this.active(0); // Inicia com o vermelho
+    } else if (this.colors[activeIndex].color === 'red') {
+      this.active(2); // Vai para o verde
+    } else if (this.colors[activeIndex].color === 'green') {
+      this.active(1); // Vai para o laranja
+    } else if (this.colors[activeIndex].color === 'orange') {
+      this.active(0); // Volta para o vermelho
+    }
+  }
+
+  active(index: number) {
+    this.colors.forEach((cor, i) => {
+      cor.active = i === index;
+    });
+    console.log(this.colors);
+  }
+  getColorClasses(cor: { color: string, active: boolean }) {
+    return {
+      'active': cor.active,
+      [cor.color]: true
+    };
+  }
+  getIcon(): string {
+    return this.init ? 'bi-stop-fill' : 'bi-play-fill';
   }
 }
