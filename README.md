a51d21531b98477c0d395beea4a326f780e69035
Taui Silva <tauisilva@gmail.com>
2024-06-03 11:04:49 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,27 +1,73 @@
-# SemaforoApp
+# Semáforo Automato Finito Determinístico
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
+## Descrição do Projeto
 
-## Development server
+Este projeto consiste na implementação de um semáforo utilizando um autômato finito determinístico (AFD). O objetivo é simular o comportamento de um semáforo de trânsito, onde o sistema pode estar em um de três estados: "Verde", "Amarelo" e "Vermelho". As transições entre esses estados seguem regras definidas que são características de um AFD.
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+![alt text](src\assets\image.png)
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## Funcionamento
 
-## Build
+O semáforo é modelado como um autômato finito determinístico com os seguintes estados e transições:
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+- **Estados**:
+  - Verde
+  - Amarelo
+  - Vermelho
 
-## Running unit tests
+- **Transições**:
+  - Verde -> Amarelo
+  - Amarelo -> Vermelho
+  - Vermelho -> Verde
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+O sistema inicia no estado "Verde" e segue as transições de forma cíclica com base em um tempo predefinido para cada estado.
 
-## Running end-to-end tests
+## Pré-requisitos
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+- [Git](https://git-scm.com)
+- [Node.js](https://nodejs.org/en/download) (v20.11, inclui npm)
 
-## Further help
+### Optional - recomendados  
+- [Github Desktop](https://desktop.github.com/)
+- [Node version manager - Windows](https://github.com/coreybutler/nvm-windows)
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+
+obs: Se estiver utilizando o [VSCode](https://code.visualstudio.com/), baixe as extensões recomendadas
+
+
+## Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+![Angular](https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white)
+![Typescript](https://img.shields.io/badge/Typescript-007acc?style=for-the-badge&labelColor=black&logo=typescript&logoColor=007acc)
+![Nodejs](https://img.shields.io/badge/Nodejs-3C873A?style=for-the-badge&labelColor=black&logo=node.js&logoColor=3C873A)
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![VSCode](https://img.shields.io/badge/Visual_Studio-0078d7?style=for-the-badge&logo=visual%20studio&logoColor=white)
+![Git](https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white)
+
+
+### ●[Angular](https://angular.dev) ● [NODE.JS](https://nodejs.org/pt-br) ● [NPM](https://www.npmjs.com/) ●  [PRIMENG](https://primeng.org/) ●  [PRIMEFLEX](https://primeflex.org/)
+
+## 🎲 Rodando a aplicação
+
+```bash
+# Clone este repositório
+$ git clone <url-do-repositorio> 
+
+# Acesse a pasta do projeto no terminal/cmd
+$ cd nome-do-projeto
+
+# Instale as dependências
+$ npm install
+
+# Execute a aplicação em modo de desenvolvimento
+$ npm start
+
+# O servidor iniciará na porta:4200 - acesse <http://localhost:4200>
+# Obs:  Não se esqueça de iniciar a API
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -8,49 +8,54 @@ import { InputNumberModule } from 'primeng/inputnumber';
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, NgClass, FormsModule,
-    InputNumberModule, FloatLabelModule
+    RouterOutlet, NgClass
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'semaforo-app';
   init = false;
   intervalId: any;
-  value: number | undefined;
+  timerId: any;
+  pedestrianRequest = false;
 
   colors = [
     { color: 'red', active: false },
-    { color: 'orange', active: false },
+    { color: 'yellow', active: false },
     { color: 'green', active: false }
   ];
 
-  cars = [
-    { moving: false, slowing: false },
-    { moving: false, slowing: false }
-  ];
+  times = {
+    green: 10000,
+    yellow: 3000,
+    red: 10000
+  };
 
-  ngOnInit(): void { }
+  currentTimes = {
+    green: 10000,
+    yellow: 3000,
+    red: 10000
+  };
+
+  constructor(private cdr: ChangeDetectorRef) { }
 
   initSemaforo() {
     this.init = true;
+    this.pedestrianRequest = false;
     this.cicloSemaforo();
-    this.intervalId = setInterval(() => {
-      this.cicloSemaforo();
-    }, this.value);
+    this.scheduleNextState();
   }
 
   stop() {
     this.init = false;
-    clearInterval(this.intervalId);
+    clearTimeout(this.intervalId);
+    clearInterval(this.timerId);
     this.colors.forEach(cor => {
       cor.active = false;
     });
-    this.cars.forEach(car => {
-      car.moving = false;
-      car.slowing = false;
-    });
+    this.resetTimes();
+    this.cdr.detectChanges();
   }
 
   cicloSemaforo() {
@@ -59,12 +64,40 @@ export class AppComponent implements OnInit {
       this.active(2); // Inicia com o verde
     } else if (this.colors[activeIndex].color === 'red') {
       this.active(2); // Vai para o verde
+      this.resetTimes();
     } else if (this.colors[activeIndex].color === 'green') {
-      this.active(1); // Vai para o laranja
-    } else if (this.colors[activeIndex].color === 'orange') {
-      this.active(0); // vai para o vermelho
+      this.active(1); // Vai para o amarelo
+    } else if (this.colors[activeIndex].color === 'yellow') {
+      this.active(0); // Volta para o vermelho
     }
-    this.updateCarsStatus();
+    this.startTimer();
+    this.cdr.detectChanges();
+  }
+
+  scheduleNextState() {
+    const activeColor = this.colors.find(cor => cor.active);
+    if (activeColor) {
+      this.intervalId = setTimeout(() => {
+        this.cicloSemaforo();
+        this.scheduleNextState();
+      }, this.currentTimes[activeColor.color as 'green' | 'yellow' | 'red']);
+    }
+  }
+
+  startTimer() {
+    clearInterval(this.timerId); // Limpa qualquer timer anterior
+    const activeColor = this.colors.find(cor => cor.active);
+    if (activeColor) {
+      const colorKey = activeColor.color as 'green' | 'yellow' | 'red';
+      this.timerId = setInterval(() => {
+        if (this.currentTimes[colorKey] > 0) {
+          this.currentTimes[colorKey] -= 1000;
+          this.cdr.detectChanges(); // Força a detecção de mudanças
+        } else {
+          clearInterval(this.timerId);
+        }
+      }, 1000);
+    }
   }
 
   active(index: number) {
@@ -73,24 +106,26 @@ export class AppComponent implements OnInit {
     });
   }
 
-  updateCarsStatus() {
-    const activeIndex = this.colors.findIndex(cor => cor.active);
-    if (this.colors[activeIndex].color === 'green') {
-      this.cars.forEach(car => {
-        car.moving = true;
-        car.slowing = false;
-      });
-    } else if (this.colors[activeIndex].color === 'orange') {
-      this.cars.forEach(car => {
-        car.moving = false;
-        car.slowing = true;
-      });
-    } else {
-      this.cars.forEach(car => {
-        car.moving = false;
-        car.slowing = false;
-      });
+  requestPass() {
+    if (!this.pedestrianRequest) {
+      this.pedestrianRequest = true;
+      const activeColor = this.colors.find(cor => cor.active);
+
+      if (activeColor?.color === 'green') {
+        this.currentTimes.green = Math.max(0, this.currentTimes.green - 4000);
+      } else if (activeColor?.color === 'red') {
+        this.currentTimes.red += 4000;
+      }
+      this.cdr.detectChanges();
     }
+  }
+
+  resetTimes() {
+    this.currentTimes.green = this.times.green;
+    this.currentTimes.yellow = this.times.yellow;
+    this.currentTimes.red = this.times.red;
+    this.pedestrianRequest = false;
+    this.cdr.detectChanges();
   }
 
   getColorClasses(cor: { color: string, active: boolean }) {
@@ -103,4 +138,13 @@ export class AppComponent implements OnInit {
   getIcon(): string {
     return this.init ? 'bi-stop-fill' : 'bi-play-fill';
   }
+
+  getRemainingTime(color: string): string {
+    const currentTime = this.currentTimes[color as 'green' | 'yellow' | 'red'];
+    const remainingSeconds = Math.floor(currentTime / 1000);
+    const formattedTime = remainingSeconds.toString().padStart(2, "0");
+    return formattedTime;
+  }
 }
+
+
